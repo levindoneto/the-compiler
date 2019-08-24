bd907482ba6306fdb45fd2f6e8d7212fe447c116
Levindo Neto <levindogtn@gmail.com>
2019-08-24 19:08:36 -0300

    Add hashtable header

---
+++ hash.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "hash.h"
+
+HASH_NODE* HashTable[HASHTABLE_SIZE];
+
+void hashInitialize (void){
+    // TODO
+}
+
+int hashGetAddress(char *text){
+    // TODO;
+}
+
+HASH_NODE* hashInsert(int type, char *text){
+    // TODO
+}
+HASH_NODE* hashFind(char *text){
+    // TODO
+}
+
+void hashPrint(void){
+    // TODO
+}
---
+++ hash.h
@@ -0,0 +1,40 @@
+#define HASHTABLE_SIZE 997 // prime number
+#define TEXT_SIZE 128
+
+typedef struct hash_node {
+    char *text;
+    int type;
+    struct hash_node *next;
+} HASH_NODE;
+
+/* Function for initializing the hash table.
+ * @parameter:  {void}.
+ * @return:     {void}.
+ */
+void hashInitialize(void);
+
+/* Function for getting an address
+ * for placing a text into the hash table.
+ * @parameter:  {string} text.
+ * @return:     {integer} address.
+ */
+int hashGetAddress(char *text);
+
+/* Function for inserting a text into the hash table.
+ * @parameter:  {integer} type.
+ * @parameter:  {string} text.
+ * @return:     {HASH_NODE} node of the hash table.
+ */
+HASH_NODE* hashInsert(int type, char *text);
+
+/* Function for finding the node where a text key is placed in.
+ * @parameter:  {string} text.
+ * @return:     {HASH_NODE} found node of the hash table.
+ */
+HASH_NODE* hashFind(char *text);
+
+/* Function for printing the whole hash table in the console.
+ * @parameter:  {void}.
+ * @return:     {void}.
+ */
+void hashPrint(void);
