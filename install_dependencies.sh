2b4d7e5f73adb884784a64e4e62c10db4c973139
Levindo Neto <levindogtn@gmail.com>
2019-08-25 20:34:48 +0000

    Add build essential to the dependencies

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,5 +1,6 @@
 echo "Installing dependencies"
 
-sudo apt-get install bison flex;
+sudo apt-get install build-essential -y; # gcc, make, etc
+sudo apt-get install bison flex -y; # lex
 
 echo "Done!"
