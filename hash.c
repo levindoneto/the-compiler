5d36deae223ee60ffe46dd1bc7abd93006d5f69a
Levindo Neto <levindogtn@gmail.com>
2019-08-26 00:43:44 +0000

    Add verifying whether value is in the hashtable before inserting it

--- hash.c
+++ hash.c
@@ -34,17 +34,20 @@ HASHTABLE_NODE* hashInsert(char *value, int type){
     HASHTABLE_NODE* newHashtableNode;
     int addressToAddNode;
     addressToAddNode = hashGetAddress(value);
-    newHashtableNode = (HASHTABLE_NODE*) calloc(DEFAULT_N_OBJECTS, sizeof(HASHTABLE_NODE*));
-    // needed allocation to avoid a missed pointer
-    newHashtableNode->value = (char*) calloc(strlen(value)+1, sizeof(char));
-    newHashtableNode->type = type;
-    // new node points to the node the last node was pointing to
-    newHashtableNode->next = Hashtable[addressToAddNode];
-    strcpy(newHashtableNode->value, value);
-    // the last node now has to point to the inserted node
-    Hashtable[addressToAddNode] = newHashtableNode;
+    if (hashFind(value) == NULL) { // it just adds to the hashtable if the value hasn't been found before
+        newHashtableNode = (HASHTABLE_NODE*) calloc(DEFAULT_N_OBJECTS, sizeof(HASHTABLE_NODE*));
+        // needed allocation to avoid a missed pointer
+        newHashtableNode->value = (char*) calloc(strlen(value)+1, sizeof(char));
+        newHashtableNode->type = type;
+        // new node points to the node the last node was pointing to
+        newHashtableNode->next = Hashtable[addressToAddNode];
+        strcpy(newHashtableNode->value, value);
+        // the last node now has to point to the inserted node
+        Hashtable[addressToAddNode] = newHashtableNode;
+        return newHashtableNode;
+    }
 
-    return newHashtableNode;
+    return hashFind(value);
 }
 
 HASHTABLE_NODE* hashFind(char *value){
@@ -57,7 +60,7 @@ HASHTABLE_NODE* hashFind(char *value){
         }
     }
 
-    return (HASHTABLE_NODE*) NODE_NOT_FOUND;
+    return NULL;
 }
 
 void hashPrint(void){
@@ -65,7 +68,7 @@ void hashPrint(void){
     int node;
     for (node = INIT_VALUE; node < HASHTABLE_SIZE; ++node){
         for (hashtableNode = Hashtable[node]; hashtableNode; hashtableNode = hashtableNode->next) {
-            fprintf(stderr, "Key: %d\nValue: %s\n#################\n", node, hashtableNode->value);
+            fprintf(stderr, "Key: %d\nValue: %s\nAddress: %p\n#################\n", node, hashtableNode->value, hashtableNode);
         }
     }
 }
