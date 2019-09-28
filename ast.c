4842fc189b0b91ca50895ca883faccb05180703c
Levindo Gabriel Taschetto Neto <levindogtn@gmail.com>
2019-09-28 20:13:21 +0000

    Add initial implementation of the AST

---
+++ ast.c
@@ -0,0 +1,12 @@
+#include "ast.h"
+
+AST_NODE* astCreate(int type, HASHTABLE_NODE* symbol, AST_NODE* son0, AST_NODE* son1, AST_NODE* son2, AST_NODE* son3) {
+  // TODO
+  AST_NODE* newnode;
+  
+  return newnode;
+}
+
+void astPrint(AST_NODE *node, int level) {
+  printf("TODO");
+}
