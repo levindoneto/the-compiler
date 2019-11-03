b692288edc58387e52d62133268403b091832798
tbarp <barpth@gmail.com>
2019-11-03 01:07:28 -0300

    Criação das funções de verificação do tipo de do retorno da função

--- semantic.c
+++ semantic.c
@@ -49,3 +49,102 @@ void checkAndSetTypes(AST*node) {
 void checkOperands(AST* node) {
     printf("TODO");
 }
+
+int isBool(AST* node){
+	if (!node) return 0;
+
+	if (node->type == AST_PARENTHESIS) return isBool(node->son[0]);
+
+	if (	node->type == AST_LESS							||
+		node->type == AST_GREATER						||
+		node->type == AST_AND							||
+		node->type == AST_OR							||
+		node->type == AST_NOT							||
+		node->type == AST_LE							||
+		node->type == AST_GE							||
+		node->type == AST_EQ							||
+		node->type == AST_DIFF							||
+		node->type == AST_NOT							||
+
+		(node->type 		== AST_SYMBOL 				&&
+		 (node->symbol->type 		==	SYMBOL_SCALAR	||	
+		  node->symbol->type 		==	SYMBOL_VECTOR	||	
+		  node->symbol->type 		==	SYMBOL_FUNCTION)	&& 
+		 node->symbol->datatype == DATATYPE_BOOL)				||
+
+		(node->type 		== AST_SYMBOL 				&&
+		 node->symbol->type 	== SYMBOL_LITBOOL)				||
+
+		(node->type 		== AST_VECTORPOS			&&
+		 node->symbol->type 	== SYMBOL_VECTOR			&&
+		 node->symbol->datatype == DATATYPE_BOOL)				||
+
+		(node->type		== AST_FUNCTIONCALL			&&
+		 node->symbol->type 	== SYMBOL_FUNCTION			&&
+		 node->symbol->datatype == DATATYPE_BOOL)
+		) return 1;
+
+	return 0;	
+}
+
+int isNotBool(AST* node){
+	if (!node) return 0;
+
+	if (node->type == AST_PARENTHESIS) return isNotBool(node->son[0]);
+
+	if (	node->type == AST_ADD							||
+		node->type == AST_SUB							||
+		node->type == AST_MUL							||
+		node->type == AST_DIV							||
+
+		(node->type 		== AST_SYMBOL 				&&
+		 (node->symbol->type 		==	SYMBOL_SCALAR	||	
+		  node->symbol->type 		==	SYMBOL_VECTOR	||	
+		  node->symbol->type 		==	SYMBOL_FUNCTION)	&& 
+		 (node->symbol->datatype == DATATYPE_INT		||
+		  node->symbol->datatype == DATATYPE_FLOAT		||
+		  node->symbol->datatype == DATATYPE_LONG))				||
+
+		(node->type 			== AST_SYMBOL 			&&
+		 (node->symbol->type 		== SYMBOL_LITINT	||
+		  node->symbol->type 		== SYMBOL_LITREAL	||
+		  node->symbol->type 		== SYMBOL_LITSTRING))			||
+
+		(node->type 			== AST_VECTORPOS		&&
+		 node->symbol->type 		== SYMBOL_VECTOR		&&
+		 (node->symbol->datatype == DATATYPE_INT		||
+		  node->symbol->datatype == DATATYPE_FLOAT		||
+		  node->symbol->datatype == DATATYPE_LONG))				||
+
+		(node->type			== AST_FUNCTIONCALL		&&
+		 node->symbol->type 		== SYMBOL_FUNCTION		&&
+		 (node->symbol->datatype == DATATYPE_INT		||
+		  node->symbol->datatype == DATATYPE_FLOAT		||
+		  node->symbol->datatype == DATATYPE_LONG))
+		) return 1;	
+
+	return 0;	
+}
+
+int confirmType(AST* node, int datatype){
+	if (!node) return 0;
+
+	if (datatype == DATATYPE_BOOL) return isBool(node->son[0]);
+	else return isNotBool(node->son[0]);
+}
+
+int checkReturn(AST* node, int datatype){
+	if (!node) return 0;
+	
+	if (node->son[0]->type == AST_RETURN) return confirmType(node->son[0], datatype);
+	else return checkReturn(node->son[1], datatype);
+}
+
+int verifyReturn(AST* node){
+	if (!node || !node->son[2] || node->son[2]->son[0]) return 0;
+	
+	if (node->son[2]->son[0]->type == AST_RETURN) return confirmType(node->son[2]->son[0], node->symbol->datatype);
+	else return checkReturn(node->son[2]->son[0], node->symbol->datatype);
+}
+
+
