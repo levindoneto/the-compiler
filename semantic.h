d03e1b8ba80d5130113cf5792c786ee9a582dc29
Levindo Gabriel Taschetto Neto <levindogtn@gmail.com>
2019-11-03 19:42:18 +0000

    Add bool functions to semantic header

--- semantic.h
+++ semantic.h
@@ -9,5 +9,10 @@ void checkUndeclared(void);
 int getSemanticErrors(void);
 void checkAndSetTypes(AST*node);
 void checkOperands(AST* node);
+int isBool(AST* node);
+int isNotBool(AST* node);
+int confirmType(AST* node, int datatype);
+int checkReturn(AST* node, int datatype);
+int verifyReturn(AST* node);
 
 #endif
