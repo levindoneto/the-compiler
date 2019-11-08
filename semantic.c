9b2ed2c8fd0ea48b3f08c02291dff4f5e733098d
tbarp <barpth@gmail.com>
2019-11-08 01:42:29 -0300

    Update semantic.c

--- semantic.c
+++ semantic.c
@@ -4,11 +4,11 @@ int errorsSemantic = INIT;
 
 AST* rootNode = 0;
 
-void checkUndeclared(void) {
+void checkUndeclared(void){
     errorsSemantic += hashGetNumberUndeclared();
 }
 
-int getNumberErrorSemantic(void) {
+int getSemanticErrors(void){
     return errorsSemantic;
 }
 
@@ -134,6 +134,7 @@ void checkOperands(AST* node) {
             		errorsSemantic++;
 		}
 		if(checkFunctionParams(node) == 0){
+            		fprintf(stderr, "Parameters not matching");
                 	errorsSemantic++;
 		}
         break;
@@ -141,12 +142,14 @@ void checkOperands(AST* node) {
 	case AST_IFTHENELSE:
 	case AST_WHILE:
 		if (isNotBool(node->son[0])){
+            		fprintf(stderr, "While wrong");
             		errorsSemantic++;
 		}
 		break;
 	case AST_LESS:
 	    for(exp = INIT; exp < MAX_COMPARE; exp++){
                 if(isBool(node->son[exp])) {
+            		fprintf(stderr, "less wrong");
                     errorsSemantic++;
                 }
             }
@@ -154,6 +157,7 @@ void checkOperands(AST* node) {
         case AST_GREATER:
             for(exp = INIT; exp < MAX_COMPARE; exp++){
                 if(isBool(node->son[exp])) {
+            		fprintf(stderr, "greater wrong");
                     errorsSemantic++;
                 }
             }
@@ -161,6 +165,7 @@ void checkOperands(AST* node) {
         case AST_LE:
             for(exp = INIT; exp < MAX_COMPARE; exp++){
                 if(isBool(node->son[exp])) {
+            		fprintf(stderr, "le wrong");
                     errorsSemantic++;
                 }
             }
@@ -168,18 +173,21 @@ void checkOperands(AST* node) {
         case AST_GE:
             for(exp = INIT; exp < MAX_COMPARE; exp++){
                 if(isBool(node->son[exp])) {
+            		fprintf(stderr, "ge wrong");
                     errorsSemantic++;
                 }
             }
             break;
         case AST_NOT:
             if(isNotBool(node->son[SON_LEFT])){
+            		fprintf(stderr, "not wrong");
                 errorsSemantic++;
             }
             break;
         case AST_AND:
             for(exp = INIT; exp < MAX_COMPARE; exp++){
                 if(isNotBool(node->son[exp])) {
+            		fprintf(stderr, "and wrong");
                     errorsSemantic++;
                 }
             }
@@ -187,29 +195,35 @@ void checkOperands(AST* node) {
         case AST_OR:
             for(exp = INIT; exp < MAX_COMPARE; exp++){
                 if(isNotBool(node->son[exp])) {
+            		fprintf(stderr, "or wrong");
                     errorsSemantic++;
                 }
             }
             break;
         case AST_DIFF:
             if(isNotBool(node->son[SON_LEFT])!=isNotBool(node->son[SON_RIGHT])){
+            		fprintf(stderr, "diff wrong");
                 errorsSemantic++;
             }
             if(isBool(node->son[SON_LEFT])!=isBool(node->son[SON_RIGHT])){
+            		fprintf(stderr, "diff wrong");
                 errorsSemantic++;
             }
             break;
         case AST_EQ:
             if(isNotBool(node->son[SON_LEFT]) != isNotBool(node->son[SON_RIGHT])){
+            		fprintf(stderr, "eq wrong");
                 errorsSemantic++;
             }
             if(isBool(node->son[SON_LEFT]) != isBool(node->son[SON_RIGHT])){
+            		fprintf(stderr, "diff wrong");
                 errorsSemantic++;
             }
             break;
 	case AST_SUB:
             for(exp = INIT; exp < MAX_COMPARE; exp++){
                 if(isBool(node->son[exp])){
+            		fprintf(stderr, "sub wrong");
                     errorsSemantic++;
                 }
             }
@@ -217,6 +231,7 @@ void checkOperands(AST* node) {
         case AST_ADD:
             for(exp = INIT; exp < MAX_COMPARE; exp++){
                 if(isBool(node->son[exp])){
+            		fprintf(stderr, "add wrong");
                     errorsSemantic++;
                 }
             }
@@ -224,7 +239,7 @@ void checkOperands(AST* node) {
         case AST_MUL:
             for(exp = INIT; exp < MAX_COMPARE; exp++){
                 if(isBool(node->son[exp])){
-fprintf(stderr, "AST_MUL");
+            		fprintf(stderr, "mul wrong");
                     errorsSemantic++;
                 }
             }
@@ -232,52 +247,57 @@ fprintf(stderr, "AST_MUL");
         case AST_DIV:
             for(exp = INIT; exp < MAX_COMPARE; exp++){
                 if(isBool(node->son[exp])){
+            		fprintf(stderr, "div wrong");
                     errorsSemantic++;
                 }
             }
             break;
         case AST_VARIABLEDECLARATION :
             if((node->symbol->type == SYMBOL_FUNCTION|| node->symbol->type == SYMBOL_VECTOR)) {
-             fprintf(stderr, "AST_VARIABLEDECLARATION");   
-errorsSemantic++;
+            		fprintf(stderr, "vardec wrong");
+                errorsSemantic++;
             }
             if (isBool(node->son[SON_LEFT])) {
+            		fprintf(stderr, "vardec wrong");
                 if(node->symbol->datatype==DATATYPE_INT ||
                    node->symbol->datatype==DATATYPE_FLOAT ||
                    node->symbol->datatype==DATATYPE_LONG ||
                    node->symbol->datatype==DATATYPE_BYTE) {
-fprintf(stderr, "AST_VARIABLEDECLARATION2");
                        errorsSemantic++;
                 }
             }
             if (isNotBool(node->son[SON_LEFT])){
+            		fprintf(stderr, "vardec wrong");
                 if(node->symbol->datatype ==DATATYPE_BOOL) {
-fprintf(stderr, "AST_VARIABLEDECLARATION3");
                     errorsSemantic++;
                 }
             }
             break;
         case AST_VECTORDECLARATION:
             if(isBool(node->son[SON_LEFT])) { // index==bool
-                fprintf(stderr, "Semantic ERROR: Index cannot be boolean. \n");
+            		fprintf(stderr, "vecdec wrong");
                 errorsSemantic++;
             }
             if(node->symbol->type != SYMBOL_VECTOR) {
-                fprintf(stderr, "Semantic ERROR: Type %d should be vector. \n", node->symbol->type);
+            		fprintf(stderr, "vecdec wrong");
                 errorsSemantic++;
             }
 		AST* auxnode = node->son[2];
 		while (auxnode){
 			if(	node->symbol->datatype != DATATYPE_BOOL && isBool(auxnode->son[0])	||
-				node->symbol->datatype == DATATYPE_BOOL && isNotBool(auxnode->son[0])) errorsSemantic++;
+				node->symbol->datatype == DATATYPE_BOOL && isNotBool(auxnode->son[0])) {errorsSemantic++;
+            			fprintf(stderr, "vecdec wrong");
+			}
 			auxnode = auxnode->son[1];
 		}
             break;
         case AST_FUNCTIONDECLARATION:
             if(node->symbol->type != SYMBOL_FUNCTION) {
+            		fprintf(stderr, "fundec wrong");
                 errorsSemantic++;
             }
 	    if(verifyReturn(node) == 0){
+            		fprintf(stderr, "return wrong");
                 errorsSemantic++;
 	    }
             break;
@@ -366,24 +386,24 @@ int isNotBool(AST* node){
 
 int confirmType(AST* node, int datatype){
 	if (!node) return 0;
-
+	
 	if (datatype == DATATYPE_BOOL) return isBool(node->son[SON_LEFT]);
 	else return isNotBool(node->son[SON_LEFT]);
 }
 
 int checkReturn(AST* node, int datatype){
-	if (!node) return 0;
-
-	if (node->son[SON_LEFT]->type == AST_RETURN) return confirmType(node->son[SON_LEFT], datatype);
+	if (!node || !node->son[SON_LEFT]) return 0;
+	
+	if (node->son[0]->type == AST_RETURN) return confirmType(node->son[0], datatype);
 	else return checkReturn(node->son[1], datatype);
 }
 
 int verifyReturn(AST* node){
-	if (!node || !node->son[2] || !node->son[2]->son[SON_LEFT]){ 
+	if (!node || !node->son[2] || !node->son[2]->son[0]){ 
 	return 0;}
 
-	if (node->son[2]->son[SON_LEFT]->type == AST_RETURN) return confirmType(node->son[2]->son[SON_LEFT], node->symbol->datatype);
-	else return checkReturn(node->son[2]->son[SON_LEFT], node->symbol->datatype);
+	if (node->son[2]->son[0]->type == AST_RETURN) return confirmType(node->son[2]->son[0], node->symbol->datatype);
+	else return checkReturn(node->son[2]->son[1], node->symbol->datatype);
 }
 
 
@@ -392,25 +412,38 @@ int checkFunctionParams(AST* node){
 
 	while (auxnode) {
 		if (auxnode->son[0]->type == AST_FUNCTIONDECLARATION) {
-			if (strcmp(auxnode->son[0]->symbol->value, node->symbol->value) == 0) break;
+			if (strcmp(auxnode->son[0]->symbol->value, node->symbol->value) == 0){
+			 break;
+			}
 		}
 		else auxnode = auxnode->son[1];
 	}
 
+	auxnode = auxnode->son[0];
+
 	if (!auxnode) return 0;
 
-	if (!auxnode->son[2] && !node->son[0]) return 1;
+	if (!auxnode->son[1] && !node->son[0]) 	return 1;
 
-	auxnode = auxnode->son[2];
+	if (!auxnode->son[1] || !node->son[0])  return 0;
+
+
+	auxnode = auxnode->son[1];
 	node = node->son[0];
 
-	while (auxnode && node){
-		if (!(	(isBool(auxnode->son[0]->son[0]) && isBool(node->son[0])) 		|| 
-			(isNotBool(auxnode->son[0]->son[0]) && isNotBool(node->son[0]))
+	while (auxnode->son[0] && node->son[0]){
+		if (!(	(auxnode->son[0]->son[0]->type == AST_BOOL && isBool(node->son[0])) 		|| 
+			(!(auxnode->son[0]->son[0]->type == AST_BOOL) && isNotBool(node->son[0]))
 		      )) return 0;
+
+	
+		if (!auxnode->son[1] && !node->son[1]) 	return 1;
+
+		if (!auxnode->son[1] || !node->son[1])  return 0;
 		auxnode = auxnode->son[1];
 		node = node->son[1];
 	}
 
 	if (!auxnode && !node) return 1;
+	return 0;
 }
