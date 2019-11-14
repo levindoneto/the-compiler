96e21fcba9620962ce9b31063363d4e54ffe5ed5
Levindo Gabriel Taschetto Neto <levindogtn@gmail.com>
2019-11-14 04:20:53 +0000

    Add node to needed tac methods

--- tac.c
+++ tac.c
@@ -7,8 +7,8 @@ TAC* functionDec(AST *node, TAC *params, TAC *block)
 {
   HASHTABLE_NODE *fBegin = makeLabel();
   HASHTABLE_NODE *fEnd = makeLabel();
-  TAC *funBegin = tacCreate(TAC_BEGINFUN, node->symbol, fBegin , 0, (int)node->type);
-  TAC *funEnd = tacCreate(TAC_ENDFUN, node->symbol, fEnd,0, 0);
+  TAC *funBegin = tacCreate(TAC_BEGINFUN, node->symbol, fBegin , 0, (int)node->type, node);
+  TAC *funEnd = tacCreate(TAC_ENDFUN, node->symbol, fEnd,0, 0, node);
   return tacJoin(tacJoin( tacJoin(funBegin,params), block), funEnd);
 }
 
@@ -75,7 +75,7 @@ TAC* makeCall(AST* node, TAC* listParam)
 {
   HASHTABLE_NODE* labelBegin = makeLabel();
   HASHTABLE_NODE* labelEnd = makeLabel();
-  TAC* callEnd = tacCreate(TAC_CALL_END, node->symbol, labelEnd, 0, 0);
+  TAC* callEnd = tacCreate(TAC_CALL_END, node->symbol, labelEnd, 0, 0, node);
 
   int numParams = (int)node->type;
   TAC *aux = listParam;
@@ -84,45 +84,44 @@ TAC* makeCall(AST* node, TAC* listParam)
 
   if (numParams == 0)
   {
-    return tacJoin(tacCreate(TAC_CALL_BEGIN, node->symbol, labelBegin,0,0), callEnd);
+    return tacJoin(tacCreate(TAC_CALL_BEGIN, node->symbol, labelBegin,0,0, node), callEnd);
   }
   else if (numParams == 1)
   {
-    return tacJoin(tacJoin(tacJoin(p, tacCreate(TAC_CALL_BEGIN, node->symbol, labelBegin,0,1)),tacCreate(TAC_CALL_PARAM, p->res, 0, 0, 0)), callEnd);
+    return tacJoin(tacJoin(tacJoin(p, tacCreate(TAC_CALL_BEGIN, node->symbol, labelBegin, 0, 1, node)),tacCreate(TAC_CALL_PARAM, p->res, 0, 0, 0, node)), callEnd);
   }
   else if (numParams > 1)
   {
     for (int i = 0; i < numParams; i++)
     {
-      p = tacJoin(p,tacCreate(TAC_CALL_PARAM, aux->res?aux->res : 0, 0, 0, numParams - i));
+      p = tacJoin(p,tacCreate(TAC_CALL_PARAM, aux->res?aux->res : 0, 0, 0, numParams - i, node));
       aux = aux->prev;
     }
   }
-  return tacJoin(tacJoin(tacCreate(TAC_CALL_BEGIN, node->symbol, labelBegin,0,(int)node->type), p), callEnd);
+  return tacJoin(tacJoin(tacCreate(TAC_CALL_BEGIN, node->symbol, labelBegin,0,(int)node->type, node), p), callEnd);
 }
 
 
-TAC* makeBinaryOperation(int op, TAC *result0, TAC *result1)
+TAC* makeBinaryOperation(int op, TAC *result0, TAC *result1, AST* node)
 {
-    TAC* opRes = tacCreate(op, makeTemp() , result0?result0->res:0, result1?result1->res:0,0);
-    return tacJoin( tacJoin(result0, result1), opRes);
-
+    TAC* opRes = tacCreate(op, makeTemp() , result0?result0->res:0, result1?result1->res:0,0, node);
+    return tacJoin(tacJoin(result0, result1), opRes);
 }
 
 
-TAC* makeIf(TAC *result0, TAC *result1)
+TAC* makeIf(TAC *result0, TAC *result1, AST* node)
 {
   TAC *newIfTac = 0;
   TAC *newLabelTac = 0;
   HASHTABLE_NODE *newLabel = 0;
   newLabel = makeLabel();
-  newIfTac = tacCreate(TAC_IFZ, result0?result0->res:0, newLabel , 0,0);
-  newLabelTac = tacCreate(TAC_LABEL, newLabel, 0, 0,0);
+  newIfTac = tacCreate(TAC_IFZ, result0?result0->res:0, newLabel, 0,0, node);
+  newLabelTac = tacCreate(TAC_LABEL, newLabel, 0, 0,0, node);
   return tacJoin(tacJoin( tacJoin(result0 , newIfTac), result1), newLabelTac);
 }
 
 
-TAC *makeIfElse(TAC *expr, TAC *cmdIf, TAC *cmdElse)
+TAC *makeIfElse(TAC *expr, TAC *cmdIf, TAC *cmdElse, AST* node)
 {
   HASHTABLE_NODE *labelElse = makeLabel();
   HASHTABLE_NODE *labelContinue = makeLabel();
@@ -130,11 +129,11 @@ TAC *makeIfElse(TAC *expr, TAC *cmdIf, TAC *cmdElse)
   TAC *lbContinue = 0;
   TAC *lbElse = 0;
   TAC *jmp = 0;
-  newIfElseTac = tacCreate(TAC_IFZ_ELSE, expr?expr->res:0, labelElse, 0,0);
-  lbContinue = tacCreate(TAC_LABEL, labelContinue, 0,0,0);
-  lbElse = tacCreate(TAC_LABEL, labelElse, 0, 0,0);
-  jmp = tacCreate(TAC_JUMP, labelContinue, 0, 0,0);
-  return tacJoin(tacJoin( tacJoin(tacJoin(tacJoin(tacJoin(expr, newIfElseTac), cmdIf), jmp), lbElse), cmdElse ),lbContinue);
+  newIfElseTac = tacCreate(TAC_IFZ_ELSE, expr?expr->res:0, labelElse, 0,0, node);
+  lbContinue = tacCreate(TAC_LABEL, labelContinue, 0,0,0, node);
+  lbElse = tacCreate(TAC_LABEL, labelElse, 0, 0,0, node);
+  jmp = tacCreate(TAC_JUMP, labelContinue, 0, 0,0, node);
+  return tacJoin(tacJoin(tacJoin(tacJoin(tacJoin(tacJoin(expr, newIfElseTac), cmdIf), jmp), lbElse), cmdElse), lbContinue);
 }
 
 void tacPrintSingle(TAC* tac)
