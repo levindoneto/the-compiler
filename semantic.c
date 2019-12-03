8100d80e584839da2895f5259a7299a1329e8b4b
Levindo Neto <levindogtn@gmail.com>
2019-12-03 00:00:36 -0300

    Add line number of the semantic error and a new test

--- semantic.c
+++ semantic.c
@@ -1,5 +1,9 @@
 #include "semantic.h"
 
+#include "y.tab.h"
+
+extern int getLineNumber();
+
 int errorsSemantic = INIT;
 AST* rootNode = 0;
 
@@ -23,7 +27,7 @@ void checkAndSetTypes(AST*node) {
 	node->type == AST_VECTORDECLARATION ||
 	node->type == AST_PARAM ) {
         if(node->symbol && node->symbol->type != SYMBOL_IDENTIFIER && node->type!= AST_PARAM) {
-            fprintf(stderr, "Semantic ERROR: Symbol %s already declared. \n", node->symbol->value);
+            fprintf(stderr, "SEMANTIC ERROR: Symbol %s already declared. LINE: %d\n", node->symbol->value, getLineNumber());
             errorsSemantic++;
         }
 	//declare symbol type and datatype
@@ -73,60 +77,60 @@ void checkOperands(AST* node) {
 	// Item 1 - Declarações
 	case AST_SYMBOL:
 		if(node->symbol->type == SYMBOL_IDENTIFIER) {
-        		fprintf(stderr, "Semantic ERROR: Symbol %s has not been declared. \n", node->symbol->value);
+        		fprintf(stderr, "SEMANTIC ERROR: Symbol %s has not been declared. LINE: %d\n", node->symbol->value, getLineNumber());
             		errorsSemantic++;
 		}
 		if (node->symbol->type == SYMBOL_FUNCTION) {
-        		fprintf(stderr, "Semantic ERROR: Symbol %s is a function and should be followed by (). \n", node->symbol->value);
+        		fprintf(stderr, "SEMANTIC ERROR: Symbol %s is a function and should be followed by (). LINE: %d\n", node->symbol->value, getLineNumber());
             		errorsSemantic++;
 		}
 		break;
 	case AST_FOR:
 		if (isNotBool(node->son[1]) || isNotBool(node->son[2])){
-        		fprintf(stderr, "Semantic ERROR: For conditions should be bool. \n");
+        		fprintf(stderr, "SEMANTIC ERROR: For conditions should be bool. LINE: %d\n", getLineNumber());
             		errorsSemantic++;
 		}
 	case AST_ATTR:
 		if (node->symbol->datatype != DATATYPE_BOOL && isBool(node->son[0])){
-        		fprintf(stderr, "Semantic ERROR: Symbol %s cannot receive boolean values. \n", node->symbol->value);
+        		fprintf(stderr, "SEMANTIC ERROR: Symbol %s cannot receive boolean values. LINE: %d\n", node->symbol->value, getLineNumber());
             		errorsSemantic++;
 		} else if (node->symbol->datatype == DATATYPE_BOOL && isNotBool(node->son[0])){
-        		fprintf(stderr, "Semantic ERROR: Symbol %s cannot receive non boolean values. \n", node->symbol->value);
+        		fprintf(stderr, "SEMANTIC ERROR: Symbol %s cannot receive non boolean values. LINE: %d\n", node->symbol->value, getLineNumber());
             		errorsSemantic++;
 		}
 	case AST_READ:
 		if (node->symbol->type != SYMBOL_SCALAR){
-        		fprintf(stderr, "Semantic ERROR: Symbol %s should be scalar in scalar attributions, reads and fors. \n", node->symbol->value);
+        		fprintf(stderr, "SEMANTIC ERROR: Symbol %s should be scalar in scalar attributions, reads and fors. LINE: %d\n", node->symbol->value, getLineNumber());
             		errorsSemantic++;
 		}
         break;
 	// Utilização de vetor
 	case AST_VECTORATTR:
 		if (node->symbol->datatype != DATATYPE_BOOL && isBool(node->son[1])){
-        		fprintf(stderr, "Semantic ERROR: Symbol %s cannot receive boolean values. \n", node->symbol->value);
+        		fprintf(stderr, "SEMANTIC ERROR: Symbol %s cannot receive boolean values. LINE: %d\n", node->symbol->value, getLineNumber());
             		errorsSemantic++;
 		} else if (node->symbol->datatype == DATATYPE_BOOL && isNotBool(node->son[1])){
-        		fprintf(stderr, "Semantic ERROR: Symbol %s cannot receive non boolean values. \n", node->symbol->value);
+        		fprintf(stderr, "SEMANTIC ERROR: Symbol %s cannot receive non boolean values. LINE: %d\n", node->symbol->value, getLineNumber());
             		errorsSemantic++;
 		}
 	case AST_VECTORPOS:
 		if (isBool(node->son[0])){
-        		fprintf(stderr, "Semantic ERROR: Symbol %s, a vector cannot have a boolean position. \n", node->symbol->value);
+        		fprintf(stderr, "SEMANTIC ERROR: Symbol %s, a vector cannot have a boolean position. LINE: %d\n", node->symbol->value, getLineNumber());
             		errorsSemantic++;
 		}
 		if (node->symbol->type != SYMBOL_VECTOR){
-        		fprintf(stderr, "Semantic ERROR: Symbol %s should be vector in vector attributions and positions. \n", node->symbol->value);
+        		fprintf(stderr, "SEMANTIC ERROR: Symbol %s should be vector in vector attributions and positions. LINE: %d\n", node->symbol->value, getLineNumber());
             		errorsSemantic++;
 		}
         break;
 	// Utilização de função
 	case AST_FUNCTIONCALL:
 		if (node->symbol->type != SYMBOL_FUNCTION){
-        		fprintf(stderr, "Semantic ERROR: Symbol %s should be function in function calls. \n", node->symbol->value);
+        		fprintf(stderr, "SEMANTIC ERROR: Symbol %s should be function in function calls. LINE: %d\n", node->symbol->value, getLineNumber());
             		errorsSemantic++;
 		}
 		if(checkFunctionParams(node) == 0){
-        		fprintf(stderr, "Semantic ERROR: Symbol %s function parameters not matching. \n", node->symbol->value);
+        		fprintf(stderr, "SEMANTIC ERROR: Symbol %s function parameters not matching. LINE: %d\n", node->symbol->value, getLineNumber());
                 	errorsSemantic++;
 		}
         break;
@@ -134,14 +138,14 @@ void checkOperands(AST* node) {
 	case AST_IFTHENELSE:
 	case AST_WHILE:
 		if (isNotBool(node->son[0])){
-        		fprintf(stderr, "Semantic ERROR: While and If Parameter should be Boolean. \n");
+        		fprintf(stderr, "SEMANTIC ERROR: While and If Parameter should be Boolean. LINE: %d\n", getLineNumber());
             		errorsSemantic++;
 		}
 		break;
 	case AST_LESS:
 	    for(exp = INIT; exp < MAX_COMPARE; exp++){
                 if(isBool(node->son[exp])) {
-        		fprintf(stderr, "Semantic ERROR: < operands should not be Boolean. \n");
+        		fprintf(stderr, "SEMANTIC ERROR: < operands should not be Boolean. LINE: %d\n", getLineNumber());
                     errorsSemantic++;
                 }
             }
@@ -149,7 +153,7 @@ void checkOperands(AST* node) {
         case AST_GREATER:
             for(exp = INIT; exp < MAX_COMPARE; exp++){
                 if(isBool(node->son[exp])) {
-        		fprintf(stderr, "Semantic ERROR: > operands should not be Boolean. \n");
+        		fprintf(stderr, "SEMANTIC ERROR: > operands should not be Boolean. LINE: %d\n", getLineNumber());
                     errorsSemantic++;
                 }
             }
@@ -157,7 +161,7 @@ void checkOperands(AST* node) {
         case AST_LE:
             for(exp = INIT; exp < MAX_COMPARE; exp++){
                 if(isBool(node->son[exp])) {
-        		fprintf(stderr, "Semantic ERROR: <= operands should not be Boolean. \n");
+        		fprintf(stderr, "SEMANTIC ERROR: <= operands should not be Boolean. LINE: %d\n", getLineNumber());
                     errorsSemantic++;
                 }
             }
@@ -165,21 +169,21 @@ void checkOperands(AST* node) {
         case AST_GE:
             for(exp = INIT; exp < MAX_COMPARE; exp++){
                 if(isBool(node->son[exp])) {
-        		fprintf(stderr, "Semantic ERROR: >= operands should not be Boolean. \n");
+        		fprintf(stderr, "SEMANTIC ERROR: >= operands should not be Boolean. LINE: %d\n", getLineNumber());
                     errorsSemantic++;
                 }
             }
             break;
         case AST_NOT:
             if(isNotBool(node->son[SON_LEFT])){
-        		fprintf(stderr, "Semantic ERROR: ~ operand should be Boolean. \n");
+        		fprintf(stderr, "SEMANTIC ERROR: ~ operand should be Boolean. LINE: %d\n", getLineNumber());
                 errorsSemantic++;
             }
             break;
         case AST_AND:
             for(exp = INIT; exp < MAX_COMPARE; exp++){
                 if(isNotBool(node->son[exp])) {
-        		fprintf(stderr, "Semantic ERROR: . operands should be Boolean. \n");
+        		fprintf(stderr, "SEMANTIC ERROR: . operands should be Boolean. LINE: %d\n", getLineNumber());
                     errorsSemantic++;
                 }
             }
@@ -187,35 +191,35 @@ void checkOperands(AST* node) {
         case AST_OR:
             for(exp = INIT; exp < MAX_COMPARE; exp++){
                 if(isNotBool(node->son[exp])) {
-        		fprintf(stderr, "Semantic ERROR: v operands should be Boolean. \n");
+        		fprintf(stderr, "SEMANTIC ERROR: v operands should be Boolean. LINE: %d\n", getLineNumber());
                     errorsSemantic++;
                 }
             }
             break;
         case AST_DIFF:
             if(isNotBool(node->son[SON_LEFT])!=isNotBool(node->son[SON_RIGHT])){
-        		fprintf(stderr, "Semantic ERROR: != operands should match. \n");
+        		fprintf(stderr, "SEMANTIC ERROR: != operands should match. LINE: %d\n", getLineNumber());
                 errorsSemantic++;
             }
             if(isBool(node->son[SON_LEFT])!=isBool(node->son[SON_RIGHT])){
-        		fprintf(stderr, "Semantic ERROR: != operands should match. \n");
+        		fprintf(stderr, "SEMANTIC ERROR: != operands should match. LINE: %d\n", getLineNumber());
                 errorsSemantic++;
             }
             break;
         case AST_EQ:
             if(isNotBool(node->son[SON_LEFT]) != isNotBool(node->son[SON_RIGHT])){
-        		fprintf(stderr, "Semantic ERROR: == operands should match. \n");
+        		fprintf(stderr, "SEMANTIC ERROR: == operands should match. LINE: %d\n", getLineNumber());
                 errorsSemantic++;
             }
             if(isBool(node->son[SON_LEFT]) != isBool(node->son[SON_RIGHT])){
-        		fprintf(stderr, "Semantic ERROR: == operands should match. \n");
+        		fprintf(stderr, "SEMANTIC ERROR: == operands should match. LINE: %d\n", getLineNumber());
                 errorsSemantic++;
             }
             break;
 	case AST_SUB:
             for(exp = INIT; exp < MAX_COMPARE; exp++){
                 if(isBool(node->son[exp])){
-        		fprintf(stderr, "Semantic ERROR: - operands should not be Boolean. \n");
+        		fprintf(stderr, "SEMANTIC ERROR: - operands should not be Boolean. LINE: %d\n", getLineNumber());
                     errorsSemantic++;
                 }
             }
@@ -223,7 +227,7 @@ void checkOperands(AST* node) {
         case AST_ADD:
             for(exp = INIT; exp < MAX_COMPARE; exp++){
                 if(isBool(node->son[exp])){
-        		fprintf(stderr, "Semantic ERROR: + operands should not be Boolean. \n");
+        		fprintf(stderr, "SEMANTIC ERROR: + operands should not be Boolean. LINE: %d\n", getLineNumber());
                     errorsSemantic++;
                 }
             }
@@ -231,7 +235,7 @@ void checkOperands(AST* node) {
         case AST_MUL:
             for(exp = INIT; exp < MAX_COMPARE; exp++){
                 if(isBool(node->son[exp])){
-        		fprintf(stderr, "Semantic ERROR: * operands should not be Boolean. \n");
+        		fprintf(stderr, "SEMANTIC ERROR: * operands should not be Boolean. LINE: %d\n", getLineNumber());
                     errorsSemantic++;
                 }
             }
@@ -239,14 +243,14 @@ void checkOperands(AST* node) {
         case AST_DIV:
             for(exp = INIT; exp < MAX_COMPARE; exp++){
                 if(isBool(node->son[exp])){
-        		fprintf(stderr, "Semantic ERROR: / operands should not be Boolean. \n");
+        		fprintf(stderr, "SEMANTIC ERROR: / operands should not be Boolean. LINE: %d\n", getLineNumber());
                     errorsSemantic++;
                 }
             }
             break;
         case AST_VARIABLEDECLARATION :
             if((node->symbol->type == SYMBOL_FUNCTION|| node->symbol->type == SYMBOL_VECTOR)) {
-        	fprintf(stderr, "Semantic ERROR: %s declaration. \n", node->symbol->value);
+        	fprintf(stderr, "SEMANTIC ERROR: %s declaration. LINE: %d\n", node->symbol->value, getLineNumber());
                 errorsSemantic++;
             }
             if (isBool(node->son[SON_LEFT])) {
@@ -254,42 +258,42 @@ void checkOperands(AST* node) {
                    node->symbol->datatype==DATATYPE_FLOAT ||
                    node->symbol->datatype==DATATYPE_LONG ||
                    node->symbol->datatype==DATATYPE_BYTE) {
-        		fprintf(stderr, "Semantic ERROR: %s value should be Boolean. \n", node->symbol->value);
+        		fprintf(stderr, "SEMANTIC ERROR: %s value should be Boolean. LINE: %d\n", node->symbol->value, getLineNumber());
                        errorsSemantic++;
                 }
             }
             if (isNotBool(node->son[SON_LEFT])){
                 if(node->symbol->datatype ==DATATYPE_BOOL) {
-        		fprintf(stderr, "Semantic ERROR: %s value should not be Boolean. \n", node->symbol->value);
+        		fprintf(stderr, "SEMANTIC ERROR: %s value should not be Boolean. LINE: %d\n", node->symbol->value, getLineNumber());
                     errorsSemantic++;
                 }
             }
             break;
         case AST_VECTORDECLARATION:
             if(isBool(node->son[SON_LEFT])) {
-        	fprintf(stderr, "Semantic ERROR: %s declaration. \n", node->symbol->value);
+        	fprintf(stderr, "SEMANTIC ERROR: %s declaration. LINE: %d\n", node->symbol->value, getLineNumber());
                 errorsSemantic++;
             }
             if(node->symbol->type != SYMBOL_VECTOR) {
-        	fprintf(stderr, "Semantic ERROR: %s declaration. \n", node->symbol->value);
+        	fprintf(stderr, "SEMANTIC ERROR: %s declaration. LINE: %d\n", node->symbol->value, getLineNumber());
                 errorsSemantic++;
             }
 		AST* auxnode = node->son[2];
 		while (auxnode){
 			if(	node->symbol->datatype != DATATYPE_BOOL && isBool(auxnode->son[0])	||
 				node->symbol->datatype == DATATYPE_BOOL && isNotBool(auxnode->son[0])) {errorsSemantic++;
-        			fprintf(stderr, "Semantic ERROR: %s values should match its type. \n", node->symbol->value);
+        			fprintf(stderr, "SEMANTIC ERROR: %s values should match its type. LINE: %d\n", node->symbol->value, getLineNumber());
 			}
 			auxnode = auxnode->son[1];
 		}
             break;
         case AST_FUNCTIONDECLARATION:
             if(node->symbol->type != SYMBOL_FUNCTION) {
-        	fprintf(stderr, "Semantic ERROR: %s declaration. \n", node->symbol->value);
+        	fprintf(stderr, "SEMANTIC ERROR: %s declaration. LINE: %d\n", node->symbol->value, getLineNumber());
                 errorsSemantic++;
             }
 	    if(verifyReturn(node) == 0){
-        	fprintf(stderr, "Semantic ERROR: %s return does not match. \n", node->symbol->value);
+        	fprintf(stderr, "SEMANTIC ERROR: %s return does not match. LINE: %d\n", node->symbol->value, getLineNumber());
                 errorsSemantic++;
 	    }
             break;
@@ -362,13 +366,13 @@ int isNotBool(AST* node){
 }
 int confirmType(AST* node, int datatype){
 	if (!node) return 0;
-	
+
 	if (datatype == DATATYPE_BOOL) return isBool(node->son[SON_LEFT]);
 	else return isNotBool(node->son[SON_LEFT]);
 }
 int checkReturn(AST* node, int datatype){
 	if (!node || !node->son[SON_LEFT]) return 1;
-	
+
 	if (node->son[0]->type == AST_RETURN) return confirmType(node->son[0], datatype);
 	else return checkReturn(node->son[1], datatype);
 }
@@ -399,7 +403,7 @@ int checkFunctionParams(AST* node){
 		if (!( (auxnode->son[0]->son[0]->type == AST_BOOL && isBool(node->son[0])) ||
 			(!(auxnode->son[0]->son[0]->type == AST_BOOL) && isNotBool(node->son[0]))
 		      )) return 0;
-	
+
 		if (!auxnode->son[1] && !node->son[1]) return 1;
 		if (!auxnode->son[1] || !node->son[1]) return 0;
 		auxnode = auxnode->son[1];
